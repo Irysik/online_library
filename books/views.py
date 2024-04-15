dc5089b2fa4df8435b2aa21bddbb5ae8638cef30
irynabodnaruk <irynabodnaruk@IFLT-00226.local>
2024-04-15 22:58:21 +0300

    Add seek

--- books/views.py
+++ books/views.py
@@ -42,24 +42,55 @@ def book_fragment(request, fragment):
     return render(request, 'book_fragment.html', {'fragment': fragment})
 
 
+# def display_first_20_lines(request, book_id):
+#     try:
+#
+#         book = Book.objects.get(pk=book_id)
+#         book_path = book.file.path
+#
+#         with open(book_path, 'r', encoding='utf-8') as file:
+#             first_20_lines = []
+#             file.seek(0)
+#             for _ in range(2000):
+#                 line = file.readline()
+#                 if not line:
+#                     break
+#                 first_20_lines.append(line)
+#
+#         return render(request, 'first_20_lines.html', {'book': book, 'first_20_lines': first_20_lines})
+#
+#     except Book.DoesNotExist:
+#         return HttpResponse('Книга не знайдена', status=404)
+#     except Exception as e:
+#         return HttpResponse(f'Помилка: {e}', status=500)
+
+#
+
 def display_first_20_lines(request, book_id):
-    try:
+    book = Book.objects.get(pk=book_id)
+    book_path = book.file.path
 
-        book = Book.objects.get(pk=book_id)
-        book_path = book.file.path
+    page_size = 5000
+    is_last_page = False
 
-        with open(book_path, 'r', encoding='utf-8') as file:
-            first_20_lines = []
-            file.seek(0)
-            for _ in range(2000):
-                line = file.readline()
-                if not line:
-                    break
-                first_20_lines.append(line)
+    page = int(request.GET.get("page", 1))
 
-        return render(request, 'first_20_lines.html', {'book': book, 'first_20_lines': first_20_lines})
+    start_symbol = (page - 1) * page_size
 
-    except Book.DoesNotExist:
-        return HttpResponse('Книга не знайдена', status=404)
-    except Exception as e:
-        return HttpResponse(f'Помилка: {e}', status=500)
+    characters = ""
+    with open(book_path, 'r', encoding='utf-8') as file:
+
+        if start_symbol > 0:
+            file.seek(start_symbol)
+        characters += file.read(page_size)
+
+    if len(characters) <= page_size:
+        is_last_page = True
+
+
+    return render(request, "first_20_lines.html", {
+                "characters": characters,
+                "is_last_page": is_last_page,
+                "page": page,
+                "book": book
+            })
